4084bf125a41ff2b0f4bfa85a34f2d29e35fbab7
Kassem <malekkassem17@gmail.cpm>
2026-05-15 02:16:04 +0200

    maison buffer

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: makassem <makassem@learner.42.tech>        +#+  +:+       +#+        */
+/*   By: malek <malek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 09:49:54 by makassem          #+#    #+#             */
-/*   Updated: 2026/05/13 16:04:36 by makassem         ###   ########.fr       */
+/*   Updated: 2026/05/14 20:55:05 by malek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,8 @@ char	*get_next_line(int fd)
 	static int		l;
 	unsigned char	*tab;
 	unsigned char	*r;
-	size_t			buffer;
 
-	buffer = 42;
 	l = 0;
-	tab = malloc(sizeof(char) * read(fd, tab, buffer));
+	tab = malloc(sizeof(char) * read(fd, tab, BUFFER_SIZE));
 	r = malloc(sizeof (char) * ft_strlen(tab));
-	
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -3,15 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: makassem <makassem@learner.42.tech>        +#+  +:+       +#+        */
+/*   By: malek <malek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 10:50:43 by makassem          #+#    #+#             */
-/*   Updated: 2026/05/13 15:19:04 by makassem         ###   ########.fr       */
+/*   Updated: 2026/05/14 20:31:22 by malek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
 
 # include <unistd.h>
 # include <stddef.h>
